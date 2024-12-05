e46b935c17ede8febfd25386d6958fa4d9e88518
laksath <166405335+laksath-affine@users.noreply.github.com>
2024-12-05 11:55:37 +0530

    pbi flask api

---
+++ pbi.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, jsonify, send_file, send_from_directory
+from flask_cors import CORS
+import matplotlib.pyplot as plt
+from io import BytesIO
+from quin_insight import quin_description
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')  # Root route to serve the HTML file
+def serve_html():
+    return send_from_directory('.', 'index.html')  # Replace 'index.html' with your actual HTML filename
+
+@app.route('/generate_data', methods=['POST'])
+def generate_data():
+    data = request.get_json()
+    query = data.get('query')
+    if not query:
+        return jsonify({'error': 'No query provided'}), 400
+
+    _, description, _ = quin_description(query)
+    print(f"Received query: {query}")
+    print(f"Received query: {description}")
+
+    try:
+        generated_data = [
+            {'description': description.split('Insights: ')[-1].split('\n')[0]}
+        ]
+    except Exception as e:
+        generated_data = [
+            {'description': 'No results found'}
+        ]
+
+    return jsonify(generated_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
